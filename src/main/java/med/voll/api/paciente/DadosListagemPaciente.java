2c646a8b441ba3aead95a25be69314a43ded90e6
wylkerd <wylkerd@hotmail.com>
2023-02-07 20:24:28 -0300

    Listagem DTO retornando id para Paciente e Medico

--- src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
@@ -1,9 +1,9 @@
 package med.voll.api.paciente;
 
-public record DadosListagemPaciente(String nome, String email, String cpf) {
+public record DadosListagemPaciente(Long id, String nome, String email, String cpf) {
 
     public DadosListagemPaciente(Paciente paciente) {
-        this(paciente.getNome(), paciente.getEmail(), paciente.getCpf());
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf());
     }
 
 }
