9de539c1b4b4de48336e8d8785139b85ad6f39f4
wylkerd <wylkerd@hotmail.com>
2023-02-07 15:22:10 -0300

    Migration com FlyWay

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -4,27 +4,32 @@ import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// Annotation para que o Spring carregue esta Classe como uma controller, ao iniciar o projeto
 @RestController
 @RequestMapping("medicos") // nomeando a URL
 public class MedicoController {
 
-    // Autowired: Annotation para Injeção de Dependencia, para criar este objeto e passar ao controller automaticamente
-    @Autowired
-    private MedicoRepository respository;  // Injetando o Repository como sendo um atriburo do Controller, denominado repository com tipo MedicoRepository
+    @Autowired // Annotation para Injeção de Dependencia, para criar este objeto e passar ao controller automaticamente
+    private MedicoRepository respository;
 
     @PostMapping
+    @Transactional // Como este é um método de escrita no BD, é necessário ter uma transação ativa com o Banco de Dados
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
         respository.save(new Medico(dados));
     }
 }
 
+// ------------ COMENTÁRIOS ----------------- //
 // @RequestBody indica que o parâmetro virá do corpo da requisição, o Body no formato JSON.
 
 // ALT + ENTER -> Create record 'DadosCadastroMedico'. Record é como uma Classe java, porém imutável, proporcionando um código menos verboso
 // (sem necessidade de Getters, equals, hashCode e toString por exemplo) se comparado a uma Classe, é um recurso das versões mais recentes.
+
+// Annotation RestController para que o Spring carregue esta Classe como uma controller, ao iniciar o projeto
+
+// Injetando o Repository como sendo um atriburo do Controller, denominado repository com tipo MedicoRepository
