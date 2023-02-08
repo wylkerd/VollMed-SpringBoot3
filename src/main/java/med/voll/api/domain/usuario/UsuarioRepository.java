708ddc44fcdb9e3568351dd3f1025e2125bb5811
wylkerd <wylkerd@hotmail.com>
2023-02-08 20:02:52 -0300

    springSecurity: Configuração de segurança para autenticação stateless. Desabilitando processo de autenticação padrão do Spring Security

--- src/main/java/med/voll/api/domain/usuario/UsuarioRepository.java
+++ src/main/java/med/voll/api/domain/usuario/UsuarioRepository.java
@@ -7,5 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     // Método findByLogin fará a consulta do usuário no Banco de Dados, na tabela usuário
+    // Usada nomenclatura para consulta dinâmica do Spring Data
     UserDetails findByLogin(String login);
+
 }
---
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -0,0 +1,29 @@
+package med.voll.api.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration // Anotação do Spring para Classe de Configuração
+@EnableWebSecurity // Anotação para indicar personalização nas configurações de segurança
+public class SecurityConfigurations {
+
+    // Configuração para aplicar processo de autenticação STATELESS
+    // Spring HttpSecurity prove métodos de configurações para o Security e permite criar um objeto Security
+    // throw Exception na assinatura do método, para lançar a Exception caso tenha uma.
+    @Bean // Serve para expor o retorno deste método ao Spring, para que devolva um objeto oo Spring automaticamente, Controller ou Classe Service
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        /*
+        * 1 - Desabilitando proteção contra ataques Cross-Site Request Forgery (CSRF), pois iremos utilizar JWT Token, que faz a proteção deste ataque
+        * 2 - sessionManagement() configura a autenticação para ser STATELESS (por padrão vem como stateful)
+        * */
+
+        return http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().build();
+    }
+
+}
