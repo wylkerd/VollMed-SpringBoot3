4b2960e700ed6aad77c6ef5f2677074865f9f1f2
wylkerd <wylkerd@hotmail.com>
2023-02-08 13:31:10 -0300

    Tratando erro 400, mostrando quais campos inválidos

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -2,10 +2,12 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-// Annotation para indicar ao Spring que esta Classe que tratará os erros, pois é do tipo RestControllerAdvice
+// Annotation para indicar ao Spring que esta Classe que tratará os erros automaticamente, pois é do tipo RestControllerAdvice
 @RestControllerAdvice
 public class TratadorDeErros {
 
@@ -13,6 +15,22 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
+    }
+
+    // Para caso no cadastro ou atualização seja encontrato erro na validação das props do Body, tratando erro 400
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException exception) {
+        var erros = exception.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList()); // mapeamento de erros para DadosErroValidacao
+    }
+
+    // DTO de resposta para indicar campos e seus erros respectivos
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            // passando para o Construtor padrão deste DTO
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 
 }
